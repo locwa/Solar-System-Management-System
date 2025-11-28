00642cb17da643b4d72ab1ca896480c37affbbda
locwa <94048557+locwa@users.noreply.github.com.>
2025-11-28 10:56:45 +0800

    added pages

---
+++ frontend/src/components/MenuBar.tsx
@@ -0,0 +1,37 @@
+import {NavLink, useNavigate} from "react-router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export function MenuBar() {
+    const navigate = useNavigate();
+
+    library.add(fas)
+    return (
+        <div className="w-[22vw] h-[100vh] bg-[#2c3e50] text-white sticky top-0 h-screen">
+            <h2 className="text-3xl text-center py-6 font-semibold">Solar System Management System</h2>
+            <hr/>
+            <ul className="py-6 px-4">
+                <li className="py-3"><NavLink to="/home"
+                                              className="w-[100%] text-left flex items-center"><FontAwesomeIcon
+                    icon={["fas", "home"]} className="pr-3"/>Home</NavLink></li>
+                <li className="py-3"><NavLink to="/inventory"
+                                              className="w-[100%] text-left flex items-center"><FontAwesomeIcon
+                    icon={["fas", "earth-asia"]} className="pr-3"/> Planets</NavLink></li>
+                <li className="py-3"><NavLink to="/add-or-remove"
+                                              className="w-[100%] text-left flex items-center"><FontAwesomeIcon
+                    icon={["fas", "check-to-slot"]} className="pr-3"/>Vote</NavLink></li>
+                <li className="py-3"><NavLink to="/item-request"
+                                              className="w-[100%] text-left flex items-center"><FontAwesomeIcon
+                    icon={["fas", "hand-holding-medical"]} className="pr-3"/>Request</NavLink></li>
+                <li className="py-3">
+                    <button
+                        className="w-[100%] text-left hover:cursor-pointer flex items-center"><FontAwesomeIcon
+                        icon={["fas", "sign-out-alt"]} className="pr-3"/> Logout
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
+
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
+import {BrowserRouter, Route, Routes} from "react-router";
 import './index.css'
-import App from './App.jsx'
+import {DashboardTemplate} from "./templates/DashboardTemplate.tsx";
+import {Login} from "./pages/Login.tsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-        <App />
+        <Routes>
+            <Route path="/" element={<DashboardTemplate/>}/>
+            <Route path="/login" element={<Login/>}/>
+        </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
